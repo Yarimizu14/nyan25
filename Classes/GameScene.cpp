59e503f17e71548e74da38b5fa3051abe31b1739
cocos2d-x <cocos2d-x@certpro106.local>
2014-01-12 17:41:54 +0900

    cardを並べる

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -22,6 +22,10 @@ bool GameScene::init()
     //set Background
     makeBackground();
     
+    srand((unsigned)time(NULL));
+    
+    makeCards();
+    
     return true;
 }
 
@@ -34,4 +38,36 @@ void GameScene::makeBackground()
     CCSprite* pBG = CCSprite::create("background.png");
     pBG->setPosition(ccp(winSize.width * 0.5, winSize.height * 0.5));
     this->addChild(pBG);
+}
+
+void GameScene::makeCards()
+{
+   //initialize array
+    CCArray* numbers = CCArray::create();
+    for(int i=1; i <= 25; i++)
+        numbers->addObject(CCInteger::create(i));
+    
+    CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+    for(int x =0; x<5; x++)
+    {
+        for(int y=0; y< 5; y++)
+        {
+            int index = rand() % numbers->count();
+            int number = ((CCInteger*)numbers->objectAtIndex(index))->getValue();
+            
+            CCSprite* pCard = CCSprite::create("card_frontside.png");
+            CCSize cardSize = pCard->getContentSize();
+            pCard->setPosition(ccp(winSize.width*0.5+(x-2)*cardSize.width, winSize.height*0.5+(y-2)*cardSize.height));
+            pCard->setTag(number);
+            this->addChild(pCard);
+            
+            CCString* fileName = CCString::createWithFormat("%d.png", number)
+            ;
+            CCSprite* pNumber =CCSprite::create(fileName->getCString());
+            pNumber->setPosition(ccp(cardSize.width * 0.5, cardSize.height * 0.5));
+            
+            pCard->addChild(pNumber);
+            numbers->removeObjectAtIndex(index);
+        }
+    }
 }
